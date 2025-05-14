f39b8ff13e8c4740723cb309b08796c0c5f20275
Arsh2236 <36arshdeep@gmail.com>
2025-05-13 23:48:15 -0600

    update

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 import csv
 
 #initialize the chrome web driver
-driver = webdriver.chrome()
+driver = webdriver.Chrome()
 
 page_url = 'https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810000401' 
 
@@ -12,7 +12,7 @@ page_url = 'https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810000401'
 driver.get(page_url)
 
 #wait for the table to load
-driver.implicity_wait(10)
+driver.implicitly_wait(10)
 
 #Get the page sorce after JavaScript excution
 page_source =  driver.page_source
@@ -42,5 +42,48 @@ if table:
     #print column headers
     print("\nRow Data:")
     print(rows)
+
+    #new set of headers inside of tuple
+    headers_tuple = (
+        'All-items', 
+        'Food5', 
+        'Shelter6', 
+        'Household operations, '
+        'furnishings and equipment', 
+        'Clothing and footwear', 
+        'Transportation', 
+        'Gasoline', 
+        'Health and personal care', 
+        'Recreation, education and reading', 
+        'Alcoholic beverages, tobacco products and recreational cannabis', 
+        'All-items excluding food and energy7', 
+        'All-items excluding energy7', 
+        'Energy7', 
+        'Goods8', 
+        'Services9'
+    )
+    
+    #write the data to csv file
+    with open('table_data.csv','w',newline="") as csvfile:
+        write = csv.writer(csvfile)
+
+        #insert the new oloumn as the first column in each row
+        for i, row in enumerate(rows):
+            rows[i] = [headers_tuple[i]] + row
+
+    # write headers
+    write.writerow(headers[2:8])
+
+    #write row
+    write.writerows(rows)
+
+    print('Data has been successfully saved to table_data.csv!')
+else:
+    print('table not found')
+
+
+    #close the webdriver
+    driver.quit( )
+
 
 
